a4622a80857262af7a378d14933e6f9e4c4f1a0c
danyirfansyah <danyirfansyah23.com>
2024-07-05 22:29:29 +0700

    New Update

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import SectionHeading from './section-heading'
+import { projectsData } from '@/lib/data'
+import Project from './project';
+
+// import { Project } from 'next/dist/build/swc';
 
 export default function Projects() {
   return (
     <section>
       <SectionHeading>My Projects</SectionHeading>
+      <div>
+        {
+          projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+          ))
+        }
+      </div>
     </section>
-  )
+  );
 }
+
+
 
